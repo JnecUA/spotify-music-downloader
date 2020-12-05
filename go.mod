914a492278a0ba128496821e6f056329cb86ca9b
Maksim <woodoomax123@gmail.com>
2020-12-05 17:28:54 +0300

    Take a request function

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/JnecUA/spotify-music-downloader
+
+go 1.15
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/geziyor/geziyor v0.0.0-20191212210344-cfb16fe1ee0e
+)
---
+++ request-sender.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/geziyor/geziyor"
+	"github.com/geziyor/geziyor/client"
+)
+
+func main() {
+	geziyor.NewGeziyor(&geziyor.Options{
+		StartRequestsFunc: func(g *geziyor.Geziyor) {
+			g.GetRendered("https://open.spotify.com/playlist/2lfAwrTGw2rbXLJf7elpo9", g.Opt.ParseFunc)
+		},
+		ParseFunc: parseSongs,
+		//BrowserEndpoint: "ws://localhost:3000",
+	}).Start()
+}
+
+func parseSongs(g *geziyor.Geziyor, r *client.Response) {
+	r.HTMLDoc.Find("div[data-testid='tracklist-row']").Each(func(i int, s *goquery.Selection) {
+
+	})
+}
