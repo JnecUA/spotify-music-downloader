fd8d918bf01aa686c5340ed644e9c6f75432727b
Maksim <woodoomax123@gmail.com>
2020-12-06 02:07:58 +0300

    Artist names, song name functions

--- request-sender.go
+++ request-sender.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/geziyor/geziyor"
 	"github.com/geziyor/geziyor/client"
 )
@@ -19,7 +18,30 @@ func main() {
 }
 
 func parseSongs(g *geziyor.Geziyor, r *client.Response) {
-	r.HTMLDoc.Find("div[data-testid='tracklist-row']").Each(func(i int, s *goquery.Selection) {
-		fmt.Println(s.Find("aria-colindex='2'").Find("span > span").Text())
-	})
+	tracks_list := strings.Split(strings.Split(string(r.Body), `"tracks"`)[1], ";") //Get track list in json format
+	tracks_list = strings.Split(strings.TrimSpace(tracks_list[0]), `"track":{`)     //Split tracks
+	tracks_list = tracks_list[1:len(tracks_list)]                                   //Delete first elemet without track
+
+}
+
+// findArtistsNames find artists names from first song in list
+func findArtistsNames(l string) string {
+	artists := strings.Split(l, `"artists"`)[1]
+	artists_names := strings.Split(artists, "name")
+	names := ""
+	for i := 1; i < len(artists_names)-1; i++ {
+		names += strings.Split(artists_names[i], `"`)[2]
+		if i != len(artists_names)-2 {
+			names += ", "
+		}
+	}
+	return names
+}
+
+//findSongName find name of first song in list
+func findSongName(l string) string {
+	track := strings.Split(l, `"artists"`)[1]
+	song := strings.Split(track, "name")
+	song = strings.Split(song[len(song)-1], `"`)
+	return song[2]
 }
