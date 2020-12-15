3f97b2b6e42b188645c59c80c926f2c96420217b
Maksim <woodoomax123@gmail.com>
2020-12-15 22:29:02 +0300

    connect go module to python

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import youtube_dl
 import urllib.request
 import urllib.parse
 import re
-
-
+import gothon
+gothon.import_hook()
+import spotify_tracklist
 def findVideoLink(search_query):
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + urllib.parse.quote_plus(search_query, safe=''))
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
     return "https://www.youtube.com/watch?v=" + video_ids[0]
-
 
 def downloadVideo(url):
     ydl_opts = {
@@ -19,4 +19,4 @@ def downloadVideo(url):
         ydl.download([url])
 
 if __name__ == "__main__":
-    pass
+    repr(spotify_tracklist)
